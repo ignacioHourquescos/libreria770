d167ea459e6a475b72b1def7ea22297d866e650c
jimancio <ihourquescos@gmail.com>
2020-09-05 01:36:23 -0300

    backlog modificado

--- js/ofertasService.js
+++ js/ofertasService.js
@@ -1,8 +1,5 @@
 // Al finalizarse de cargar el DOM:
 
-var server ="http://localhost:5000";
-
-
 function cargarOfertas () {
 		//busca en el backend todas las ofertas
 		$.getJSON(('https://spreadsheets.google.com/feeds/list/1OS6vvVKksK6UptapP20_P6Q1TAPOk4QR6p2eslv9XKE/1/public/values?alt=json'), function (data) {
@@ -16,12 +13,14 @@ function cargarOfertas () {
 				});
 			insertarDatosEnNuevoDiv(divCompetencia,data,i);
 		}
-		$("#plantilla").remove();
+		$("#plantilla").remove(); // remueve la plnatilla que se carga con la pagina
 	});
 }
   
 function insertarDatosEnNuevoDiv(elemento,data,i){
 	var idColor = 1;
+	//trae data del backend desde un google sheets y lo va insertando en el titulo, el detalle, el precio etc.
+	//data.feed.entry[i].gsx$promo.$t y similares es la manera de acceder al dato que nos da google sheets. No asustarse!!
 	$(elemento).find('.titulo').text(data.feed.entry[i].gsx$promo.$t);
 	$(elemento).find('.detalle').text(data.feed.entry[i].gsx$descripcion.$t);
 	$(elemento).find('.precio').text(data.feed.entry[i].gsx$precio.$t);
