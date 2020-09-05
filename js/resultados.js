2eada30a459267115f6c9bf5b31e7b9f76aa3e5f
jimancio <ihourquescos@gmail.com>
2020-09-05 01:34:23 -0300

    version backlog modificado

--- js/resultados.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-	var idCompetencia = getQueryParam("id");
-	var competenciasController = new CompetenciasController();
-	competenciasController.obtenerResultados(idCompetencia);
-});
