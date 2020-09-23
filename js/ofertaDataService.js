d6b36340f37273714ba3c8c4ceafc19c605ce5c2
jimancio <ihourquescos@gmail.com>
2020-09-23 15:13:52 -0300

    ofertasData fucnionando con dos listas

--- js/ofertaDataService.js
+++ js/ofertaDataService.js
@@ -1,6 +1,32 @@
+var listadoUrl=[
+	{
+		lista:"Normal",
+		url:"https://docs.google.com/spreadsheets/d/1OS6vvVKksK6UptapP20_P6Q1TAPOk4QR6p2eslv9XKE/edit?usp=sharing"
+	},
+	{
+		lista:"Warnes",
+		url:"https://docs.google.com/spreadsheets/d/1R5PAunP3ppaS2SOcTf6z2_BTOBlWxAIED1zxWnEg1us/edit?usp=sharing"
+	}
+
+]
+
+function cambiarLista(){
+var listaSeleccionada=document.getElementById("seleccionLista");
+document.getElementById("seleccionLista").onchange= function imprimirPorPantalla(){
+	for (var i =0; i<listadoUrl.length;i++){
+		if(listadoUrl[i].lista==listaSeleccionada.value)
+		var listaAMostrar = listadoUrl[i].url;
+	}
+	document.getElementById("iframe").src = concatenar(listaAMostrar);
+}
+}
 
 
 
+function concatenar(url){
+	let formato="&rm=minimal&amp;range=A1:C3&amp;single=true&amp;headers=false&amp;widget=false&amp;chrome=false";
+	let urlFormateado =url+formato;
+	return(urlFormateado);
+}
 
-urlLista1 =https://docs.google.com/spreadsheets/d/1OS6vvVKksK6UptapP20_P6Q1TAPOk4QR6p2eslv9XKE/edit?usp=sharing
-urlLista2 =https://docs.google.com/spreadsheets/d/1R5PAunP3ppaS2SOcTf6z2_BTOBlWxAIED1zxWnEg1us/edit?usp=sharing
+cambiarLista();
