de5d7cb19d2614600982498bd6074547986705cc
jimancio <ihourquescos@gmail.com>
2020-10-27 00:10:49 -0300

    cambie ofertas data font

--- js/ofertaDataService.js
+++ js/ofertaDataService.js
@@ -1,10 +1,10 @@
 var listadoUrl=[
 	{
-		lista:"Normal",
+		lista:"Oficina & Hogar",
 		url:"https://docs.google.com/spreadsheets/d/1OS6vvVKksK6UptapP20_P6Q1TAPOk4QR6p2eslv9XKE/edit?usp=sharing"
 	},
 	{
-		lista:"Warnes",
+		lista:"Embalaje",
 		url:"https://docs.google.com/spreadsheets/d/1R5PAunP3ppaS2SOcTf6z2_BTOBlWxAIED1zxWnEg1us/edit?usp=sharing"
 	}
 
